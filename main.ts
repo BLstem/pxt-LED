3a5d0fe696b1970bdd1084ad9daa4619aaf785a4
DoraLC <34760377+DoraLC@users.noreply.github.com>
2020-07-06 15:32:47 +0800

    Update pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
-basic.forever(function () {
-	
-})
+//%color=#DAF7A6 icon="\uf0eb" block="LED"
+
+namespace LED{
+    let strip = neopixel.create(DigitalPin.P1, 12, NeoPixelMode.RGB)
+    strip.showColor(neopixel.rgb(0, 0, 0))
+
+    //%block="Breathing Lights speed $speed
+    //%speed.min=0 speed.max=100
+    export function breathinglights(speed: number=100): void{
+        let time = 105 - speed
+        for (let index = 0; index <= 255; index++) {
+        strip.showColor(neopixel.rgb(index, 0, 0))
+        basic.pause(time)
+        }
+        for (let index = 0; index <= 255; index++) {
+        strip.showColor(neopixel.rgb(255 - index, 0, 0))
+        basic.pause(time)
+        }
+    }
+}
