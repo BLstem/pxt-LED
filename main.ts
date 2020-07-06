f5c682c9d4755b4d8feffb32379912d05070879f
DoraLC <34760377+DoraLC@users.noreply.github.com>
2020-07-06 15:44:03 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ namespace LED{
     let strip = neopixel.create(DigitalPin.P1, 12, NeoPixelMode.RGB)
     strip.showColor(neopixel.rgb(0, 0, 0))
 
-    //%block="Breathing Lights speed $speed
+    //%block="Breathing Lights speed $speed"
     //%speed.min=0 speed.max=100
     export function breathinglights(speed: number=100): void{
         let time = 105 - speed
