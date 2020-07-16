9f2951aca82420b2a76c450f0e902d9c4df9afa6
DoraLC <34760377+DoraLC@users.noreply.github.com>
2020-07-16 19:50:22 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,9 +4,9 @@ namespace LED{
     let strip = neopixel.create(DigitalPin.P1, 12, NeoPixelMode.RGB)
     strip.showColor(neopixel.rgb(0, 0, 0))
 
-    //%block="Breathing Lights speed $speed"
+    //%block="Breathing Lights (RED) speed $speed"
     //%speed.min=0 speed.max=100
-    export function breathinglights(speed: number=100): void{
+    export function breathinglightsred(speed: number=100): void{
         let time = 105 - speed
         for (let index = 0; index <= 255; index++) {
         strip.showColor(neopixel.rgb(index, 0, 0))
@@ -14,6 +14,34 @@ namespace LED{
         }
         for (let index = 0; index <= 255; index++) {
         strip.showColor(neopixel.rgb(255 - index, 0, 0))
+        basic.pause(time)
+        }
+    }
+
+    //%block="Breathing Lights (GREEN) speed $speed"
+    //%speed.min=0 speed.max=100
+    export function breathinglightsgreen(speed: number=100): void{
+        let time = 105 - speed
+        for (let index = 0; index <= 255; index++) {
+        strip.showColor(neopixel.rgb(0, index, 0))
+        basic.pause(time)
+        }
+        for (let index = 0; index <= 255; index++) {
+        strip.showColor(neopixel.rgb(0, 255 - index, 0))
+        basic.pause(time)
+        }
+    }
+
+    //%block="Breathing Lights (BLUE) speed $speed"
+    //%speed.min=0 speed.max=100
+    export function breathinglightsgreenblue(speed: number=100): void{
+        let time = 105 - speed
+        for (let index = 0; index <= 255; index++) {
+        strip.showColor(neopixel.rgb(0, 0, index))
+        basic.pause(time)
+        }
+        for (let index = 0; index <= 255; index++) {
+        strip.showColor(neopixel.rgb(0, 0, 255 - index))
         basic.pause(time)
         }
     }
